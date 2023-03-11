8d55bacf321aa815f2f7c31f61f221632ee10210
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-03-11 19:57:47 +0300

    Add: Route handler

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { connect, getModels } = require("./database/dbConfig");
-
-//Import routes
-const v1OrganizationRouter = require("./v1/routes/organizationRoutes");
-const v1SupplierRouter = require("./v1/routes/supplierRoutes");
-const v1AircraftRouter = require("./v1/routes/aircraftRoutes");
-const v1EngineerRouter = require("./v1/routes/engineerRoutes");
-const v1ClientRouter = require("./v1/routes/clientRoutes");
-const v1UserRouter = require("./v1/routes/userRoutes");
-const v1UnitRouter = require("./v1/routes/unitRoutes");
-const v1PartRouter = require("./v1/routes/partRoutes");
+const routes = require("./v1/routes");
 
 const env = process.env;
 const app = express();
@@ -21,15 +12,8 @@ app.use(function attachModels(req, res, next) {
   next();
 });
 
-//App routes
-app.use("/api/v1/organizations", v1OrganizationRouter);
-app.use("/api/v1/suppliers", v1SupplierRouter);
-app.use("/api/v1/aircrafts", v1AircraftRouter);
-app.use("/api/v1/engineers", v1EngineerRouter);
-app.use("/api/v1/clients", v1ClientRouter);
-app.use("/api/v1/users", v1UserRouter);
-app.use("/api/v1/units", v1UnitRouter);
-app.use("/api/v1/parts", v1PartRouter);
+// App routes
+app.use(routes);
 
 const PORT = env.PORT || 3300;
 app.listen(PORT, () => {
---
+++ src/v1/routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// Import route handlers
+const v1UserRouter = require("./userRoutes");
+const v1UnitRouter = require("./unitRoutes");
+const v1PartRouter = require("./partRoutes");
+const v1ClientRouter = require("./clientRoutes");
+const v1SupplierRouter = require("./supplierRoutes");
+const v1AircraftRouter = require("./aircraftRoutes");
+const v1EngineerRouter = require("./engineerRoutes");
+const v1OrganizationRouter = require("./organizationRoutes");
+
+// Use route handlers
+router.use("/api/v1/users", v1UserRouter);
+router.use("/api/v1/units", v1UnitRouter);
+router.use("/api/v1/parts", v1PartRouter);
+router.use("/api/v1/clients", v1ClientRouter);
+router.use("/api/v1/suppliers", v1SupplierRouter);
+router.use("/api/v1/aircrafts", v1AircraftRouter);
+router.use("/api/v1/engineers", v1EngineerRouter);
+router.use("/api/v1/organizations", v1OrganizationRouter);
+
+module.exports = router;
