a8a554dce7d080d306536c42fe2468ec0dcfc870
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-04-21 10:39:19 +0300

    Add: Issue part schema

---
+++ src/schemas/issuePartSchema/index.js
@@ -0,0 +1,15 @@
+const yup = require("yup");
+
+const issuePartSchema = yup.object({
+    date_of_issue: yup.date().required("Required"),
+    part_id: yup.string().required("Required"),
+    description: yup.string().required("Required"),
+    serial_number: yup.string().required("Required"),
+    part_status: yup.string().required("Required"),
+    quantity_on_hand: yup.number().positive().required("Required"),
+    quantity_issued: yup.number().positive().required("Required"),
+    sp_in_usd: yup.number().positive().required("Required"),
+    engineer_id: yup.string().required("Required"),
+});
+
+module.exports = { issuePartSchema };
