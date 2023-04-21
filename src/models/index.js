a14616fd93d1379454dcfb6491419d89bfc6aff2
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-04-21 10:19:25 +0300

    Update: Setup models function

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,47 @@
-const { modelsAssociations } = require("./modelsAssociations");
+const { jobAssociations } = require("./jobModel/associations");
+const { partAssociations } = require("./partModel/associations");
+const { unitAssociations } = require("./unitModel/associations");
+const { userAssociations } = require("./userModel/associations");
+const { clientAssociations } = require("./clientModel/associations");
+const { engineerAssociations } = require("./engineerModel/associations");
+const { aircraftAssociations } = require("./aircraftModel/associations");
+const { purchaseAssociations } = require("./purchaseModel/associations");
+const { supplierAssociations } = require("./supplierModel/associations");
+const { issuedPartAssociations } = require("./issuedPartModel/associations");
+const { organizationAssociations } = require("./organizationModel/associations");
+const { purchaseHistoryAssociations } = require("./purchaseHistoryModel/associations");
+const { issuedPartHistoryAssociations } = require("./issuedPartHistoryModel/associations");
 
 function setupModels(sequelize) {
     try {
-        const associations = modelsAssociations(sequelize);
+        const Job = jobAssociations(sequelize);
+        const Unit = unitAssociations(sequelize);
+        const User = userAssociations(sequelize);
+        const Part = partAssociations(sequelize);
+        const Client = clientAssociations(sequelize);
+        const Aircraft = aircraftAssociations(sequelize);
+        const Purchase = purchaseAssociations(sequelize);
+        const Engineer = engineerAssociations(sequelize);
+        const Supplier = supplierAssociations(sequelize);
+        const IssuedPart = issuedPartAssociations(sequelize);
+        const Organization = organizationAssociations(sequelize);
+        const PurchaseHistory = purchaseHistoryAssociations(sequelize);
+        const IssuedPartHistory = issuedPartHistoryAssociations(sequelize);
 
         return {
-            ...associations,
+            ...Job,
+            ...Unit,
+            ...User,
+            ...Part,
+            ...Client,
+            ...Aircraft,
+            ...Purchase,
+            ...Engineer,
+            ...Supplier,
+            ...IssuedPart,
+            ...Organization,
+            ...PurchaseHistory,
+            ...IssuedPartHistory,
         };
     } catch (err) {
         throw err;
