4732432011e1ef2f0c00411cd143d92fd8e7a1a3
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-04-26 18:18:40 +0300

    Update: Delete one issued part by organization id

--- src/database/IssuedPart/deleteOneIssuedPart.js
+++ src/database/IssuedPart/deleteOneIssuedPart.js
@@ -1,5 +1,53 @@
+const { sequelize } = require("../dbConfig");
+const { IssuedPart } = require("./issuedPartModel");
+const { findItem } = require("../helpers/findItem");
+const { PartQuantity } = require("../PartQuantity/partQuantityModel");
+const { checkOrganization } = require("../helpers/checkOrganization");
+const {
+    IssuedPartHistory,
+} = require("../issuedPartHistory/issuedPartHistoryModel");
+
 const deleteOneIssuedPart = async (organizationId, issuedPartId) => {
-    return;
+    let transaction;
+    try {
+        transaction = await sequelize.transaction();
+        await checkOrganization(organizationId);
+
+        const findPart = "an issued part";
+        await findItem(IssuedPart, findPart, issuedPartId, organizationId);
+
+        const issuedPartHistory = await IssuedPartHistory().findOne({
+            where: { issued_part_id: issuedPartId },
+            transaction,
+        });
+
+        const updatePartQuantity = {
+            quantity_issued: -issuedPartHistory.quantity_issued,
+            quantity_on_hand: -issuedPartHistory.quantity_issued,
+        };
+
+        await PartQuantity().increment(
+            { ...updatePartQuantity },
+            { where: { part_id: issuedPartHistory.part_id }, transaction }
+        );
+
+        await IssuedPart().destroy({
+            where: { id: issuedPartId, organization_id: organizationId },
+            transaction,
+        });
+
+        await IssuedPartHistory().destroy({
+            where: { issued_part_id: issuedPartId },
+            transaction,
+        });
+
+        await transaction.commit();
+    } catch (error) {
+        if (transaction) {
+            transaction.rollback();
+        }
+        throw { status: error?.status || 500, message: error?.message || error };
+    }
 };
 
 module.exports = { deleteOneIssuedPart };
--- src/services/issuedPartService/deleteOneIssuedPart.js
+++ src/services/issuedPartService/deleteOneIssuedPart.js
@@ -1,7 +1,15 @@
 const IssuedPart = require("../../database/IssuedPart");
 
 const deleteOneIssuedPart = (organizationId, issuedPartId) => {
-    return;
+    try {
+        const issuedPart = IssuedPart.deleteOneIssuedPart(
+            organizationId,
+            issuedPartId
+        );
+        return issuedPart;
+    } catch (error) {
+        throw error;
+    }
 };
 
 module.exports = { deleteOneIssuedPart };
