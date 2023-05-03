7be83a720ae63e5e5abb4599200074cb51c01c47
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-05-03 07:55:34 +0300

    Update: Send access token to user

--- src/controllers/authController/login.js
+++ src/controllers/authController/login.js
@@ -5,13 +5,14 @@ const login = async (req, res) => {
     const body = req.body;
     try {
         const loggedInUser = await authService.login(body);
-        const accessToken = createToken({ id: loggedInUser.id });
+        const claims = { id: loggedInUser.id };
+        const accessToken = createToken(claims); //Serialize user using the secrect key
 
         res.cookie("access-token", accessToken, {
             httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
-        res.status(200).send({ auth: true, data: loggedInUser });
+        res.status(200).send({ auth: true, token: accessToken });
     } catch (error) {
         res.status(error?.status || 500).send({
             auth: false,
